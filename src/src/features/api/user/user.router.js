a7c3452cd72c2a310df80388fe8d25f0b04c6171
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-11-02 13:16:04 +0000

    Initial Commit

---
+++ src/src/features/api/user/user.router.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const router = express.Router();
+
+const authorization = require('../../../utils/middleware/authorization');
+const userController = require('./user.controller');
+const middleware = require('./user.middleware');
+const validator = require('./user.validator');
+
+// Ver un usuario
+router.get('/:userUuid', authorization('users:view'), middleware.loadUser, userController.getUser);
+
+// Listar los usuarios paginados
+router.get('/', authorization('users:view'), userController.listUsers);
+
+// Crear un usuario
+router.post('/', authorization('users:create'), validator.createUser, userController.createUser);
+
+// Editar un usuario
+router.put(
+  '/:userUuid',
+  authorization('users:update'),
+  validator.putUser,
+  middleware.loadUser,
+  userController.putUser,
+);
+
+// Borrar un usuario
+router.delete(
+  '/:userUuid',
+  authorization('users:delete'),
+  validator.deleteUser,
+  middleware.loadUser,
+  userController.deleteUser,
+);
+
+module.exports = router;
---
+++ src/src/models/index.js
@@ -0,0 +1,24 @@
+const sequelize = require('sequelize');
+const Sequelize = require('../config/db');
+
+const Activity = require('./activity');
+const User = require('./user');
+const UserGroup = require('./userGroup');
+
+const models = {
+  Activity: Activity.init(Sequelize, sequelize),
+  User: User.init(Sequelize, sequelize),
+  UserGroup: UserGroup.init(Sequelize, sequelize),
+};
+
+Object.values(models)
+  .filter((model) => typeof model.associate === 'function')
+  .forEach((model) => model.associate(models));
+
+const db = {
+  ...models,
+  Sequelize,
+};
+
+// We export the sequelize connection instance to be used around our app.
+module.exports = db;
