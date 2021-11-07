16bb68ea7f578c2ebe11a33c1df2159d18320ad4
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-11-07 21:28:03 +0000

    add seed for user billing table creation

--- src/src/seeders/20211104231050-demo-user-billing.js
+++ src/src/seeders/20211104231050-demo-user-billing.js
@@ -1,4 +1,6 @@
 const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+const { UserBilling, UserGroup } = require('../models');
 
 module.exports = {
   up: async (queryInterface) => {
@@ -15,11 +17,27 @@ module.exports = {
           address: 'Telde',
         },
       ]);
+
+      const password = bcrypt.hashSync('123456', bcrypt.genSaltSync(10));
+      const studentsGroup = await UserGroup.findOne({ where: { name: 'Students' } });
+      const lasPalmasBilling = await UserBilling.findOne({ where: { address: 'Las Palmas' } });
+
+      await queryInterface.bulkInsert('users', [
+        {
+          uuid: uuid.v4(),
+          name: 'sofia',
+          email: 'sofia@gmail.com',
+          password,
+          role_uuid: studentsGroup.uuid,
+          token: '',
+          active: true,
+          billing_id: lasPalmasBilling.uuid,
+        },
+      ]);
     } catch (error) {
       console.log(error);
     }
   },
-
   down: async (queryInterface) => {
     await queryInterface.bulkDelete('user_billings', null, {});
   },
