a9002469370e82854a0b719c9dfed0b56d386278
Ana Valdemoro Ramirez <ana.valdemoro@agiliacenter.com>
2022-01-13 10:58:09 +0000

    FTR: posibility to search cart by userUuid added

--- src/src/features/api/cart/cart.filters.js
+++ src/src/features/api/cart/cart.filters.js
@@ -2,29 +2,13 @@ const { Op } = require('sequelize');
 
 module.exports = (params) => {
   const query = {};
-  const orConditions = [];
 
-  if (params.name) {
-    orConditions.push({
-      name: {
-        [Op.like]: `%${params.name}%`,
-      },
-    });
+  if (params.userUuid) {
+    query.user_uuid = {
+      [Op.eq]: params.userUuid,
+    };
+  }
 
-    orConditions.push({
-      '$billing.dni$': {
-        [Op.like]: `%${params.name}%`,
-      },
-    });
-    orConditions.push({
-      '$billing.address$': {
-        [Op.like]: `%${params.name}%`,
-      },
-    });
-  }
-  if (orConditions.length !== 0) {
-    query[Op.or] = orConditions;
-  }
   query.deleted = { [Op.ne]: true };
 
   return query;
